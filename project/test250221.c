12a518ef7de51c06f554c3c429d043247d1d0951
to-4 <to-4@example.com>
2025-03-07 09:34:40 +0900

    work-20250307

---
+++ project/test250221.c
@@ -0,0 +1,11 @@
+#include<stdio.h>  // Include standard input-output header file
+
+// Main function: Entry point of the program
+int main() 
+{
+    // Print "Hello, World!!" to the console
+    printf("Hello, World!!\n");
+    
+    // Return 0 to indicate successful execution
+    return 0;
+}
